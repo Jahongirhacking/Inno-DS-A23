bc1f9a4cae42dcd45bc5487d686e47ca11ea2a07
Jahongir Hayitov <66916141+Jahongirhacking@users.noreply.github.com>
2023-02-22 19:51:30 +0300

    Accepted solution

--- Coding Exercises Week 5/problemB.cpp
+++ Coding Exercises Week 5/problemB.cpp
@@ -3,36 +3,55 @@
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
 
-map<string, bool> DP;
+// Dictionary check
+map<string, bool> dictionary;
+// Check whether we already check it
+vector<bool> DP;
+stack<string> result;
 
-
-void findWord(string s, vector<string> arr) {
-    string temp = "";
-    for(int i=0; i<s.size(); i++) {
-        temp += s[i];
-        if(DP[temp]) {
-            arr.push_back(temp);
-            if(i == s.size()-1) {
-                for(string str:arr) if(str!="") cout<<str<<" ";
-                exit(0);
-            }
-            findWord(s.substr(i+1), arr);
-            arr.pop_back();
+bool checkWordFromDict(string& str, int index) {
+    // If the whole string exists in Dictionary
+    if(str.length() <= index) return true;
+    // If we took it already
+    if(DP[index]) return false;
+    // We take it
+    DP[index] = true;
+    for (int i = 1; index+i <= str.length(); i++) {
+        string prefixOfString = str.substr(index, i);
+        // first check prefix, if it exists in dictionary
+        if(dictionary.count(prefixOfString)>0 && checkWordFromDict(str, index+i)) {
+            // If we find the path we need to push all prefix
+            result.push(prefixOfString);
+            return true;
         }
     }
+    // the prefix is not in Dictionary
+    return false;
 }
 
 int main() {
-    int n, k;
-    string str;
+    // n - number of dictionary words
+    // k - characters
+    int n;
+    int k;
+    string word;
     cin >> n >> k;
-    // store dictionary words
-    for(int i=0; i<n; i++) {
-        cin >> str;
-        DP[str] = true;
+    // input dictionary words
+    rep(i, n) {
+        cin >> word;
+        dictionary[word] = true;
     }
-    cin >> str;
-    findWord(str, {});
+    cin >> word;
+    
+    DP.resize(k, false);
+    // check and store until you find path
+    checkWordFromDict(word, 0);
+    // Output the words
+    while(!result.empty()) {
+        cout << result.top() << " ";
+        result.pop();
+    }
     return 0;
 }

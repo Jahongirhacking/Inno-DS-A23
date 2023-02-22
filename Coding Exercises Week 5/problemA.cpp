9dda80b35371f954cb252923f8599c92315440c4
Jahongir Hayitov <66916141+Jahongirhacking@users.noreply.github.com>
2023-02-22 04:18:03 +0300

    Accepted using Bottom-Up approach

--- Coding Exercises Week 5/problemA.cpp
+++ Coding Exercises Week 5/problemA.cpp
@@ -5,59 +5,53 @@ typedef long long ll;
 typedef unsigned long long ull;
 #define rep(i,n) for (ll i = 0; i < (n); ++i)
 
-vector<pair<int, int>> items;
-map<pair<int, int>, pair<vector<int>, int>> dp; // index and weight
-
-// Knapsack algorithm that returns
-// vector - positions, and int - cost
-pair<vector<int>, int> knapsack(int weight, int index) {
-    if(index == -1 || weight == 0) return {{}, 0}; // position array and cost
-    // check from exist items
-    if(dp.count({index, weight}) > 0) {
-        return dp[{index, weight}];
-    }
-    // if item's weight doesn't fit
-    // then go to next item
-    if(weight < items[index].first) {
-        dp[{index, weight}] = knapsack(weight, index-1);
-        return dp[{index, weight}];
-    }
-    // if item's weight fits
-    // We have 2 choices: yes or no
-    pair<vector<int>, int> temp1 = knapsack(weight, index-1); // no choice
-    pair<vector<int>, int> temp2 = knapsack(weight-items[index].first, index-1); // yes choice
-
-    // choose the greatest
-    if(temp1.second > temp2.second + items[index].second) {
-        dp[{index, weight}] = temp1;
-        return temp1;
-    } else {
-        temp2.first.push_back(index);
-        temp2.second += items[index].second;
-        dp[{index, weight}] = temp2;
-        return temp2;
-    }
-}
-
 int main() {
     // number of elements and knapsack weight
-    int n, w;
-    cin >> n >> w;
-    // array for weight and costs
-    items.resize(n, {0, 0});
+    int n, weight;
+    cin >> n >> weight;
+    // array for weight and costs for each item
+    vector<pair<int, int>> items(n, {0, 0}); // weight and cost
     rep(i, n) cin >> items[i].first;
     rep(i, n) cin >> items[i].second;
 
-    int index = n-1;
-    pair<vector<int>, int> pos;
-    // find the positions
-    pos = knapsack(w, index);
+    vector<int> empty(weight+1, 0);
+    vector<vector<int>> DP(n+1, empty);
+    // Maximum cost properties
+    int maxCost = -1;
+    int maxPosY, maxPosX;
+    // Find the maximum by DP
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=weight; j++) {
+            // out of bound exception
+            if(j-items[i-1].first < 0) {
+                DP[i][j] = max(DP[i-1][j], 0);
+            }
+            else {
+                DP[i][j] = max(DP[i-1][j], DP[i-1][j-items[i-1].first]+items[i-1].second);
+            }
+            // Assign maximum
+            if(DP[i][j] > maxCost) {
+                maxCost = DP[i][j];
+                maxPosX = j;
+                maxPosY = i;
+            }
+        }
+    }
 
-    // output
-    cout << pos.first.size() << endl;
-    for(int i : pos.first) cout<<i+1<<" ";
+    // Find the path via backtracking
+    vector<int> result;
+    while(DP[maxPosY][maxPosX] != 0) {
+        if(DP[maxPosY][maxPosX] != DP[maxPosY-1][maxPosX]) {
+            maxPosX -= items[maxPosY-1].first;
+            result.push_back(maxPosY);
+        }
+        maxPosY--;
+    }
+
+    // Output
+    cout << result.size() << endl;
+    for(int i = result.size()-1; i>=0; i--) {
+        cout << result[i] << " ";
+    }
     return 0;
 }
-
-// 0.5 s
-// Time Limit Test 54

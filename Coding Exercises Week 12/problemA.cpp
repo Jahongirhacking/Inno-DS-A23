ebdc76e03db63769ca8431ede065b87aa987a37f
Jahongir Hayitov <66916141+Jahongirhacking@users.noreply.github.com>
2023-04-12 05:53:07 +0300

    MST full version (PriorityQueue implemented)

--- Coding Exercises Week 12/problemA.cpp
+++ Coding Exercises Week 12/problemA.cpp
@@ -3,6 +3,64 @@ using namespace std;
 typedef long long ll;
 #define INF 1e18
 
+template <typename W, typename I>
+class PriorityQueueMIN {
+public:
+    PriorityQueueMIN() = default;
+    void push(pair<W, pair<I, I>> costBetweenTwoCities) {
+        this->arr.push_back(costBetweenTwoCities);
+        this->size++;
+        // keep track of the last added element
+        ll index = this->size-1;
+        while(index-1>=0 && this->arr[(index-1)/2].first > this->arr[index].first) {
+            swap(this->arr[(index-1)/2], this->arr[index]);
+            index = (index-1)/2;
+        }
+    }
+
+    void pop() {
+        swap(arr[0], arr[this->size-1]);
+        this->arr.pop_back();
+        this->size--;
+        // keep track of the new first element
+        ll index = 0;
+        // repeat until there is no child
+        while(index*2+1 < this->size) {
+            ll left = index*2+1, right = index*2+2;
+            bool flag = false;
+            // if both children exist
+            if(right < this->size) {
+                // if the right child is less than the left
+                if(arr[right].first < arr[left].first) {
+                    if(arr[right] < arr[index]) {
+                        swap(arr[right], arr[index]);
+                        index = right;
+                        flag = true;
+                    } else return;
+                }
+            } 
+            if(flag) continue;
+            // if the left child is less than or equal the right
+            if(arr[left] < arr[index]) {
+                swap(arr[left], arr[index]);
+                index = left;
+            } else return;
+        }
+    }
+
+    bool empty() {
+        if(this->size == 0) return true;
+        return false;
+    }
+
+    pair<W, pair<I, I>> top() {
+        return this->arr[0];
+    }
+    
+    vector<pair<W, pair<I, I>>> arr;
+    ll size = 0;
+};
+
 template<typename S, typename P>
 class Vertex {
 public:
@@ -52,23 +110,28 @@ public:
         for(ll start=0; start < numberOfVertex; start++){
             if(processed[start]) continue;
             costs[start] = 0;
-            this->PriorityQueue.push(make_pair(costs[start], make_pair(-1, start)));
+            this->PriorityQueue->push(make_pair(costs[start], make_pair(-1, start)));
             // Check its adjacent vertices
-            while (!PriorityQueue.empty()) {
-                pair<double, pair<ll, ll>> TOP = PriorityQueue.top();
+            while (!PriorityQueue->empty()) {
+                pair<double, pair<ll, ll>> TOP = PriorityQueue->top();
                 ll fromCityIndex = TOP.second.second;
-                PriorityQueue.pop();
+                PriorityQueue->pop();
+                // if already visted, ignore
                 if (processed[fromCityIndex]) continue;
+                // make it visited
                 processed[fromCityIndex] = true;
+                // If it's just one vertex do NOT print
                 if (TOP.second.first != -1)
                     cout << vertices[TOP.second.first]->name
                          << ":" << vertices[TOP.second.second]->name << " ";
+                // Visit adjacent vertices
                 for (auto toCity: adj[fromCityIndex]) {
                     ll toCityIndex = toCity->toCityIndex;
                     double weight = toCity->weight;
+                    // if conditions hold then put it to the priority queue
                     if (!processed[toCityIndex] && weight < costs[toCityIndex]) {
                         costs[toCityIndex] = weight;
-                        PriorityQueue.push({-weight, make_pair(fromCityIndex, toCityIndex)});
+                        PriorityQueue->push({weight, make_pair(fromCityIndex, toCityIndex)});
                     }
                 }
             }
@@ -76,10 +139,10 @@ public:
         cout<<endl;
     }
 
-    priority_queue<pair<double,pair<ll,ll>>> PriorityQueue; // weight
+    PriorityQueueMIN<double, ll> *PriorityQueue = new PriorityQueueMIN<double, ll>(); // weight, {index1, index2}
     map<S, ll> getIndex; // name, index of vertex
-    vector<V*> vertices; // name, penalty
-    map<ll, vector<E*>> adj; // index, edge
+    vector<V*> vertices; // Vertex
+    map<ll, vector<E*>> adj; // index, Edge
     ll numberOfVertex = 0;
 };
 
